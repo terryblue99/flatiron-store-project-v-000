993279149c2df3f158c7cd0add4c23426db95bf5
Terry Blue <terryblue@comcast.net>
2017-10-12 17:36:15 +0000

    Passed user_spec.rb

---
+++ db/migrate/20171012172115_add_current_cart_id_to_user.rb
@@ -0,0 +1,5 @@
+class AddCurrentCartIdToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :current_cart_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171011212925) do
+ActiveRecord::Schema.define(version: 20171012172115) do
 
   create_table "carts", force: :cascade do |t|
     t.integer  "user_id"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20171011212925) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "current_cart_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
